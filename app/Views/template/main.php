8b8fdfa88fd3d3a9c68c9f3f9788dd75151cd707
Moisés David Ramón Esteban <masemoyv1@gmail.com>
2024-03-16 22:58:39 -0600

    Barra lateral

--- app/Views/template/main.php
+++ app/Views/template/main.php
@@ -13,7 +13,7 @@
 
     <style {csp-style-nonce}>
         body {
-            background-color: rgb(0,92,171);
+            background-color: rgb(0, 92, 171);
         }
     </style>
 
@@ -26,9 +26,18 @@
     <div class="wrapper">
         <aside id="sidebar" class="js-sidebar">
             <!-- Content For Sidebar -->
+
+            <div class="pb-2 mb-2 border-bottom border-white">
+                <!-- <img src="https://upn113leon.edu.mx/wp-content/uploads/2022/08/UPN-png.png" alt="Logo_Upn_Oficial" width="50px" height="50px" style="margin-left:-180px;margin-top:-380px"> -->
+            </div>
             <div class="h-100">
                 <div class="sidebar-logo">
-                    <a href="#"><?= session()->get('nombre')?></a>
+                    <a href="#">
+                        <?= session()->get('nombre') . " " . session()->get('apaterno') . " " . session()->get('amaterno') ?>
+                    </a>
+                    <a href="#">
+                        <?= session()->get('identificador') ?>
+                    </a>
                 </div>
                 <ul class="sidebar-nav">
                     <li class="sidebar-header">
@@ -36,63 +45,48 @@
                     </li>
                     <li class="sidebar-item">
                         <a href="#" class="sidebar-link">
-                            <i class="fa-solid fa-person-chalkboard pe-2"></i></i>
-                            Competencias
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-house" viewBox="0 0 16 16">
+                                <path
+                                    d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
+                            </svg>
+                            Inicio
                         </a>
                     </li>
 
                     <li class="sidebar-item">
                         <a href="<?= base_url('admin/usuarios') ?>" class="sidebar-link">
-                            <i class="fa-solid fa-users pe-2"></i>
-                            Usuarios
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-person" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                            </svg>
+                            Perfil
                         </a>
                     </li>
 
-
-
-                    
                     <li class="sidebar-item">
-                        <a href="#" class="sidebar-link collapsed" data-bs-target="#auth" data-bs-toggle="collapse"
-                            aria-expanded="false"><i class="fa-solid fa-sliders pe-2"></i>
-                            Módulos
+                        <a href="#" class="sidebar-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-mortarboard" viewBox="0 0 16 16">
+                                <path
+                                    d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917zM8 8.46 1.758 5.965 8 3.052l6.242 2.913z" />
+                                <path
+                                    d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46z" />
+                            </svg>
+                            Mis cursos
                         </a>
-                        <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                            <li class="sidebar-item">
-                                <a href="<?= base_url('admin/carreras') ?>" class="sidebar-link">Carreras</a>
-                            </li>
-                            <li class="sidebar-item">
-                                <a href="<?= base_url('admin/asignaturas') ?>" class="sidebar-link">Asignaturas</a>
-                            </li>
-
-
-
-                            <li class="sidebar-item">
-                                <a href="#" class="sidebar-link">Grupos</a>
-                            </li>
-                        </ul>
                     </li>
-                    <li class="sidebar-header">
-                        Multi Level Menu
-                    </li>
+
                     <li class="sidebar-item">
-                        <a href="#" class="sidebar-link collapsed" data-bs-target="#multi" data-bs-toggle="collapse"
-                            aria-expanded="false"><i class="fa-solid fa-share-nodes pe-2"></i>
-                            Multi Dropdown
+                        <a href="#" class="sidebar-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-people" viewBox="0 0 16 16">
+                                <path
+                                    d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
+                            </svg>
+                            Grupos
                         </a>
-                        <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                            <li class="sidebar-item">
-                                <a href="#" class="sidebar-link collapsed" data-bs-target="#level-1"
-                                    data-bs-toggle="collapse" aria-expanded="false">Level 1</a>
-                                <ul id="level-1" class="sidebar-dropdown list-unstyled collapse">
-                                    <li class="sidebar-item">
-                                        <a href="#" class="sidebar-link">Level 1.1</a>
-                                    </li>
-                                    <li class="sidebar-item">
-                                        <a href="#" class="sidebar-link">Level 1.2</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
                     </li>
                 </ul>
             </div>
@@ -111,7 +105,8 @@
 
                         <li class="nav-item">
                             <a class="nav-link" data-toggle="dropdown" href="#">
-                                <i class="fas fa-user"></i> <?= session()->get('email') ?>
+                                <i class="fas fa-user"></i>
+                                <?= session()->get('email') ?>
                             </a>
 
                         </li>
@@ -133,10 +128,10 @@
 
 
                     <div>
-                        <?= $this->renderSection('content') ?>    
+                        <?= $this->renderSection('content') ?>
                     </div>
 
-                    
+
                 </div>
             </main>
             <a href="#" class="theme-toggle">
@@ -149,7 +144,8 @@
                         <div class="col-6 text-start">
                             <p class="mb-0">
                                 <a href="#" class="text-muted">
-                                    Developed for Ingeniería Informática ITST. <?= date('Y') . '.'; ?>
+                                    Developed for Ingeniería Informática ITST.
+                                    <?= date('Y') . '.'; ?>
                                 </a>
                             </p>
                         </div>
@@ -186,7 +182,9 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
