70ca632810a521af8a898b8e093c363f192f6cc1
Moisés David Ramón Esteban <masemoyv1@gmail.com>
2024-05-02 20:41:05 -0600

    Modelo funcional en RegistroController

--- app/Controllers/RegistroController.php
+++ app/Controllers/RegistroController.php
@@ -14,7 +14,7 @@ class RegistroController extends BaseController
 
     public function __construct()
     {
-        $this->usuarioModel = new UsuarioModel();
+        $this->usuarioModel = model('UsuarioModel');
     }
 
 
