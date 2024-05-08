fc1417ebc4ec46f09fc61e94432092fb9ecdf684
tsunami159 <106128942+tsunami159@users.noreply.github.com>
2024-05-08 16:41:02 -0600

    Add files via upload

--- app/Controllers/Estudiante/MisCursosController.php
+++ app/Controllers/Estudiante/MisCursosController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers\Estudiante;
 
 use App\Controllers\BaseController;
+use App\Models\SubtemaModel;
+use App\Models\ContenidoModel;
 
 class MisCursosController extends BaseController
 {
@@ -14,5 +16,45 @@ class MisCursosController extends BaseController
             return redirect('/');
         }
         return view ('estudiante/misCursos/index');
+    }
+
+    public function subtema1()
+    {
+        $subtemaModel = new SubtemaModel();
+        $subtemaData = $subtemaModel->where('idSubtema', 1)->first();
+        $contenidoModel = new ContenidoModel();
+        $contenidoData = $contenidoModel->where('idContenido', $subtemaData->idContenido)->first();
+        $data = [
+            'subtemaData' => $subtemaData,
+            'contenidoData' => $contenidoData
+        ];
+
+        return view('estudiante/misCursos/subtema1', $data);
+    }
+    public function subtema2()
+    {
+        $subtemaModel = new SubtemaModel();
+        $subtemaData = $subtemaModel->where('idSubtema', 2)->first();
+        $contenidoModel = new ContenidoModel();
+        $contenidoData = $contenidoModel->where('idContenido', $subtemaData->idContenido)->first();
+        $data = [
+            'subtemaData' => $subtemaData,
+            'contenidoData' => $contenidoData
+        ];
+    
+        return view('estudiante/misCursos/subtema2', $data);
+    }
+    
+    public function subtema3()
+    {
+        return view ('estudiante/misCursos/subtema3');
+    }
+    public function subtema4()
+    {
+        return view ('estudiante/misCursos/subtema4');
+    }
+    public function evaluacion()
+    {
+        return view ('estudiante/misCursos/evaluacion');
     }
 }
