db367247be7a3b6d1d6d501516be6a7d753302ee
MoisesDavidRE <masemoyv1@gmail.com>
2024-05-20 16:13:23 +0000

    Actualizar cambios

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://192.168.8.7:80/';
+    public string $baseURL = 'http://172.16.21.167:80/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
